6ba7bb4b49977cd8d4c7406bcd40dd7b9a438679
An B. Luu <109339483+TechnoMechno@users.noreply.github.com>
2023-01-06 15:51:11 +0700

    Uploaded the Utilities class

---
+++ UTILITIES.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "LIBDECLARATIONS.h"
+
+vector<string> split(string arg, string delimiter) {
+	/*
+	NOTE:
+		- Declare variables for iteration
+			+ `temp` to insert interated characters in to form a string
+			+ `i` for iteration counter
+			+ retvec for the vector to be returned
+	*/
+
+	string temp = "";
+	int i = 0;
+	vector<string> retvec;
+
+	/*
+	NOTE:
+		- Iterate over `arg` string
+
+		- Insert iterated characters into `temp`
+
+		- When encounter 1st character of delimiter, insert `temp` into retvec,
+		skip i over delimiter length. Delimiter is resetted.
+
+		- When encounter last character of `arg`, insert current temp into retvec
+	*/
+	while (i < arg.length()) {
+		if (arg[i] == delimiter[0]) {
+			retvec.push_back(temp);
+			temp = "";
+			i = i + delimiter.length();
+			continue;
+		}
+
+		else {
+			temp += arg[i];
+			if (i == arg.length() - 1) {
+				retvec.push_back(temp);
+			}
+			i++;
+		}
+	}
+
+	return retvec;
+}
