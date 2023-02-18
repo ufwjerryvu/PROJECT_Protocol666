b8c17c034fcae208039b21cbed8d64a5f8ca6a88
Kenny Nguyen <113963528+KhNg-Ken@users.noreply.github.com>
2023-02-18 17:57:02 +0700

    Implemented the RangeGoon's detect function.

---
+++ Source/RangeGoon.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Enemy.h"
+
+class RangeGoon : public Enemy {
+public:
+	void attack();
+	void detectPlayer(Player& arg);
+};
+
+void RangeGoon::attack() {
+
+}
+
+void RangeGoon::detectPlayer(Player& arg) {
+	const int DETECTION_RANGE = 300;
+	const int ATTACK_RANGE = 200;
+
+	/*
+	NOTE:
+		- Similar to MeleeGoons, RangeGoons detect Player in a rectangular box 
+		in the direction the MeleeGoon is facing.
+
+		- RangeGoons also share the rectangular detection zone with MeleeGoon, 
+		which is defined by the height of the EnemyGoon x 300 pixels in length
+		(half and a little less than the width of the game window).
+
+		- Once the player is detected, the RangeGoon will not shoot immediately
+		as a shooting range of 300 pixels might seem unfair. Hence, it will have
+		to run towards the Player, until it is in range to attack.
+
+		- DEVELOPMENT: ATTACK_RANGE needs to be properly develop once 
+		projectiles are done.
+	*/
+
+	if (arg.getY() > this->getY() && arg.getY() < this->getY() - this->getHeight()) {
+		if (this->getDirectionFacing() == Direction::RIGHT) {
+			if (arg.getX() > this->getX() && arg.getX() && arg.getX() < this->getX() + DETECTION_RANGE) {
+				this->setX(this->getX() + 6);
+
+				if (this->getX() + ATTACK_RANGE == arg.getX()) {
+					this->attack();
+				}
+			}
+		}
+		else if (this->getDirectionFacing() == Direction::LEFT) {
+			if (arg.getX() < this->getX() && arg.getX() > this->getX() - DETECTION_RANGE) {
+				this->setX(this->getX() - 6);
+
+				if (this->getX() + ATTACK_RANGE == arg.getX()) {
+					this->attack();
+				}
+			}
+		}
+	}
+}
